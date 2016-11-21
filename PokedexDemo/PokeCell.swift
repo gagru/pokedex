130958943f029ff75d3fbe8522a39de0db40435b
administrator <ttdat.0410@gmail.com>
2016-11-21 17:26:06 +0700

    commit new UI

---
+++ PokedexDemo/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  PokedexDemo
+//
+//  Created by Administrator on 11/19/16.
+//  Copyright © 2016 Administrator. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}
---
+++ PokedexDemo/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokedexDemo
+//
+//  Created by Administrator on 11/19/16.
+//  Copyright © 2016 Administrator. All rights reserved.
+//
+
+import Foundation
